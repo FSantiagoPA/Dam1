4b57b0c9fb2aec2f1a063cb99038a52bd92b9953
Farid <santiago123.fspr@gmail.com>
2024-02-16 19:20:10 +0100

    update Tema03Bucles

---
+++ src/main/java/consantiagocom/PrimerTrimestre/Tema03Bucles/Ejercicio07.java
@@ -0,0 +1,22 @@
+package consantiagocom.PrimerTrimestre.Tema03Bucles;
+
+import java.util.Scanner;
+
+public class Ejercicio07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double base;
+        int exponente;
+        boolean exponenteNegativo = false;
+        double potencia = 1;
+
+        System.out.println("Indica la base");
+        base = scanner.nextDouble();
+        System.out.print("Indica el exponente: ");
+        exponente = scanner.nextInt();
+        scanner.close();
+
+
+    }
+}
