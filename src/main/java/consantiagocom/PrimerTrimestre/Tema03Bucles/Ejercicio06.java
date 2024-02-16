e3cb751a45bb3852a60d0c9d6f5f130ec5968012
Farid <santiago123.fspr@gmail.com>
2024-02-16 18:44:06 +0100

    update Tema03Bucles

--- src/main/java/consantiagocom/PrimerTrimestre/Tema03Bucles/Ejercicio06.java
+++ src/main/java/consantiagocom/PrimerTrimestre/Tema03Bucles/Ejercicio06.java
@@ -1,8 +1,23 @@
 package consantiagocom.PrimerTrimestre.Tema03Bucles;
 
+import java.util.Scanner;
+
 public class Ejercicio06 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numMultiplicador;
 
+        System.out.print("Ingrese un numero: ");
+        numMultiplicador = scanner.nextInt();
+        System.out.println("*** Tabla de multiplicar del " + numMultiplicador + " ***");
+        if (numMultiplicador >= 0) {
+            for (int i = 1; i <= 10; i++) {
+                int resultado = numMultiplicador * i;
+                System.out.println(numMultiplicador + " * " + i + " = " + resultado);
+            }
+        }else {
+            System.err.println("El numero ingresado no es valido!");
+        }
 
     }
 }
