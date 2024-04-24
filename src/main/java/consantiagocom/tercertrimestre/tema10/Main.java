75f7e63c45ad5a1993dcfcd83636dd09164a20e1
santiago <santiago123.fspr@gmail.com>
2024-04-24 13:07:31 +0200

    Update tema10

--- src/main/java/consantiagocom/tercertrimestre/tema10/Main.java
+++ src/main/java/consantiagocom/tercertrimestre/tema10/Main.java
@@ -4,12 +4,16 @@ import consantiagocom.tercertrimestre.tema10.ejercicio01.Ej1;
 import consantiagocom.tercertrimestre.tema10.ejercicio02.Ej02;
 import consantiagocom.tercertrimestre.tema10.ejercicio03.Ej03;
 import consantiagocom.tercertrimestre.tema10.ejercicio04.Ej04;
+import consantiagocom.tercertrimestre.tema10.ejercicio05.Ej05;
+import consantiagocom.tercertrimestre.tema10.ejercicio06.Ej06;
 
 public class Main {
     public static void main(String[] args) {
         //Ej1 ej1 = new Ej1();
         //Ej02 ej02 = new Ej02();
-        Ej03 ej03 = new Ej03();
+        //Ej03 ej03 = new Ej03();
         //Ej04 ej04 = new Ej04();
+        Ej05 ej05 = new Ej05();
+        //Ej06 ej06 = new Ej06();
     }
 }
---
+++ src/main/java/consantiagocom/tercertrimestre/tema10/ejercicio05/Paciente.java
@@ -0,0 +1,79 @@
+package consantiagocom.tercertrimestre.tema10.ejercicio05;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Paciente {
+
+    //Configurando variables constantes
+    private static final String DEFAULT_GENDER  = "M";
+    static int localId = 0;
+
+    //Atributos
+    private int id;
+    private String nombre;
+    private char sexo;
+    private Date fechaNacimiento;
+    private double altura;
+    private double peso;
+
+    public Paciente(String nombre, Date fechaNacimiento, char sexo , double altura, double peso) {
+        this.id = localId++;
+        this.nombre = nombre;
+        this.sexo = sexo;
+        this.fechaNacimiento = fechaNacimiento;
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    //Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    @Override
+    public String toString() {
+        return "Paciente{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", sexo=" + sexo +
+                ", fechaNacimiento=" + fechaNacimiento +
+                ", altura=" + altura +
+                ", peso=" + peso +
+                "}\n";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Paciente paciente = (Paciente) o;
+        return id == paciente.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+}
